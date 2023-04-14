7e2f6f251d24f0a0304fac1877bb80c0e469f8da
Danyelle Brown <124711752+DanyBee7@users.noreply.github.com>
2023-04-14 20:57:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var output = [];
+function fizzBuzz() {
+    for (var count = 1; count < 101; count++) {
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push("FizzBuzz");
+        } else if (count % 3 === 0 ) {
+            output.push("Fizz");
+        } else if (count % 5 === 0) {
+            output.push("Buzz");
+        } else {
+            output.push(count);
+        }
+    }
+    console.log(output);
+}
+fizzBuzz();
